11aa446b1aaa9a8667e63f1594b903dc39f300f8
John Fawcett <john.fawcett@kyrus-tech.com>
2014-05-28 10:26:21 -0600

    Run file command and parse type

--- bootcamp/__init__.py
+++ bootcamp/__init__.py
@@ -1,4 +1,9 @@
 import logging
+import os
+
+from utils import *
+
+THIS_DIR = os.path.realpath(os.path.dirname(__file__))
 
 
 class Extractomatic(object):
@@ -12,3 +17,34 @@ class Extractomatic(object):
         Extracts files from the given archive
         """
         logging.info('Extracting [%s]', archive)
+
+        fullpath = os.path.realpath(archive)
+
+        if not os.path.exists(fullpath):
+            logging.error('Could not find [%s]', archive)
+            return
+
+        proc_input = {}
+        proc_input['filename'] = fullpath
+
+        name, extension = os.path.splitext(fullpath)
+
+        logging.info('Extracting [%s] with extension [%s]', archive, extension)
+
+        output = ''
+        with lcd(THIS_DIR):
+            output = local('file %s' % fullpath)
+            logging.debug('file command output: %s', output)
+
+        # Output will look similar to this:
+        # /Users/jfawcett/git/apk-sploder/apks/fmlife.activities.apk: Zip archive data, at least v2.0 to extract
+        file_output = '\\n'.join(output).split(fullpath)[1][2:]
+
+        # file_output should now look like this:
+        # Zip archive data, at least v2.0 to extract
+        logging.debug('file command split output: %s', file_output)
+        file_type = file_output.split(' ')[0].lower()
+        logging.debug('file type: %s', file_type)
+        # if file_type == 'zip':
+        #
+        # elif file_type == 'awef':
