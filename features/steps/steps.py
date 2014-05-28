0088effdeaf81e635c1946763b6c79c526241dd2
Leslie Minor <Leslie@Leslies-MacBook-Pro.local>
2014-05-28 12:11:27 -0600

    Added prelim testing stubs for gzip and zip

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -13,3 +13,31 @@ def step_impl(context, args):
 def step_impl(context):
     # TODO: implement
     assert False
+
+@given(u'the file is a "zip"')
+def step_impl(context):
+    assert False
+
+@when(u'given to an "zip_extractor"')
+def step_impl(context):
+    assert False
+
+@then(u'the file should be "directory with unzipped archive file"')
+def step_impl(context):
+    assert False
+
+@given(u'the file is a "gpg"')
+def step_impl(context):
+    assert False
+
+@then(u'the file should be "exception"')
+def step_impl(context):
+    assert False
+
+@given(u'the file is a "gzip"')
+def step_impl(context):
+    assert False
+
+@when(u'given to an "gzip_extractor"')
+def step_impl(context):
+    assert False
